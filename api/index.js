ad21a33c3b277da690efeff746380806db22632f
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2025-05-30 10:51:06 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ let db;
 async function connectMongoDB() {
   try {
     const client = await MongoClient.connect(mongoUri, { useUnifiedTopology: true });
-    db = client.db('sample_mflix');
+    db = client.db('saviorsann');
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
